f087f97c0138f1967dcf855e1f67ce1f505ee80f
LilasAll <lilas.allard@gmail.com>
2019-11-04 17:47:52 +0100

    second commit

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENGRID_LOGIN'],
+  :password => ENV['SENGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
