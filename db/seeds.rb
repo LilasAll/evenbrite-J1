755bf56141480b35e72b48377018c70d9bb9f59b
LilasAll <lilas.allard@gmail.com>
2019-11-05 15:02:58 +0100

    adding event on first page

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,17 +21,17 @@ Event.destroy_all
 Attendance.destroy_all
 puts "Last database destroy"
 
-5.times do 
-	User.create!(email: "user#{rand(1..100)}@yopmail.com",encrypted_password: Faker::Creature::Animal.name,description: Faker::Lorem.paragraph_by_chars(number: 100, supplemental: false),first_name: Faker::Name.first_name,last_name: Faker::Name.last_name)
+8.times do 
+	User.create!(email: "user#{rand(1..100)}@yopmail.com",password: Faker::Lorem.paragraph_by_chars(number: 8, supplemental: false),description: Faker::Lorem.paragraph_by_chars(number: 100, supplemental: false),first_name: Faker::Name.first_name,last_name: Faker::Name.last_name)
 end
 puts "User created"
 
-3.times do
+8.times do
 	Event.create!(start_date: Faker::Date.forward(days: 250),duration: rand(1..10)*5,title: Faker::Lorem.paragraph_by_chars(number: 30, supplemental: false),description: Faker::Lorem.paragraph_by_chars(number: 400, supplemental: false),price: Faker::Number.between(from: 2, to: 999),location: random_city,admin: User.all.sample)
 end
 puts "Event created"
 
-5.times do
+8.times do
 	Attendance.create!(user_id: User.all.sample.id,event_id: Event.all.sample.id,stripe_customer_id: Faker::Alphanumeric.alpha(number: 6))
 end
 puts "Attendance created"
