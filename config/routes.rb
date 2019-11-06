c8145c1890a1b8e287090a801564ddecf29fe19f
LilasAll <lilas.allard@gmail.com>
2019-11-06 13:49:21 +0100

    Adding stripe payement

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :event
   resources :user
+  resources :charges
   get '/', to: 'event#index'
 
 end
